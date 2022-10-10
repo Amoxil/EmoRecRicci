b3e060beb5546686952c7886b4be75a1fe1cf94f
Amoxil <raffaele_zheng@libero.it>
2022-10-10 19:50:38 +0200

    Added normalization for curvature values

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,5 +1,9 @@
 import cv2
 import os
+import pandas
+import numpy
+from sklearn.preprocessing import MinMaxScaler
+from ricci_graph_builder import FACE_EDGES
 
 def processImages(dir, labels):
     face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
@@ -37,4 +41,20 @@ def processImages(dir, labels):
                     resizedImage = cv2.resize(face, (128,128))
                     image.split()
                     cv2.imwrite(imagePath, resizedImage)
-            
+
+def normalizeDf(df):
+    #BE SURE TO REMOVE DATAFRAME HEADER!!! (Or first entry will not be considered)
+    images = df.iloc[:, :1]
+    norm = df.iloc[: , 1:-1]
+    label = df.iloc[:,-1:]
+
+
+    scaler = MinMaxScaler()
+    #print(norm)
+    norm = scaler.fit_transform(norm)
+    norm = pandas.DataFrame(norm, columns=numpy.arange(len(FACE_EDGES)))
+    #print(images)
+    result = pandas.concat([images, norm], axis=1, join="inner")
+    result = pandas.concat([result, label], axis=1, join="inner")
+    #print(result)
+    result.to_csv('normalizedData.csv', index=False, header=False)
