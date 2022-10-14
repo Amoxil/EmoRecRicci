2a1dcb74daa182647c051d16bf0960748b952846
Amoxil <raffaele_zheng@libero.it>
2022-10-14 20:12:35 +0200

    Added multiple selection models for testing

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ import feature_extraction_dlib
 import networkx
 import cv2
 import pandas
+import training
 
-dataFrame = feature_extraction_dlib.extractFeatureFrom("C:\\Users\\Raffocs\\Desktop\\CK+", ["anger", "contempt", "disgust", "fear", "happiness", "neutral", "sadness", "surprise"])
+#dataFrame = feature_extraction_mp.extractFeatureFrom("C:\\Users\\Raffocs\\Desktop\\CK+", ["anger", "contempt", "disgust", "fear", "happiness", "neutral", "sadness", "surprise"])
+
+training.trainTestHoldOut("dataDlib.csv")
--- training.py
+++ training.py
@@ -1,27 +1,95 @@
+from asyncio.windows_utils import pipe
 from dataclasses import dataclass
 from operator import index
 from xml.etree.ElementTree import tostring
 import pandas
+import numpy
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, KFold, cross_val_score, LeaveOneOut, StratifiedKFold, RepeatedKFold
 from sklearn.metrics import accuracy_score
 
-ricciCurvData = pandas.read_csv("normalizedDataManhattan.csv")
-print(ricciCurvData)
-df = ricciCurvData.iloc[: , 1:-1]
-labels = ricciCurvData.iloc[:,-1:]
+def trainTestKFold(data):
 
-i=0
-sum=0
+    
+    kFold = KFold(n_splits=10)
+    ricciCurvData = pandas.read_csv(data, header=None)
+    print(ricciCurvData)
+    classifier = RandomForestClassifier(n_estimators=50, random_state=1)
+    df = ricciCurvData.iloc[: , 1:-1]
+    labels = ricciCurvData.iloc[:,-1:]
 
-while(i<100):
-    dfTrain, dfTest, labelsTrain, labelsTest = train_test_split(df, labels, test_size=0.3)
+    """
+    scores = []
+    for train_set, test_set in kFold.split(df):
+        classifier.fit(df.loc[train_set], labels.loc[train_set].values.ravel())
+        score = classifier.score(df.loc[test_set], labels.loc[test_set].values.ravel())
+        print(df.loc[test_set], labels.loc[test_set])
+        print("-----------------------------------------------------------")
+        scores.append(score)
+    
+    print(scores)
+    print(numpy.array(scores).mean())
+    """
+    kfResults = cross_val_score(estimator=classifier, X=df, y=labels.values.ravel(), scoring="accuracy", cv=kFold)
+    print(kfResults)
+    print(kfResults.mean())
 
-    model = DecisionTreeClassifier()
-    model.fit(dfTrain, labelsTrain)
-    predictions = model.predict(dfTest)
-    sum = sum + accuracy_score(labelsTest, predictions)
-    print("Accuracy: " + str(accuracy_score(labelsTest, predictions)*100))
-    i=i+1
+def trainTestStratKFold(data):
 
-print("Average accuracy over 100 tests: " + str(sum))
+    
+    stratKFold = StratifiedKFold(n_splits=10)
+    ricciCurvData = pandas.read_csv(data, header=None)
+    print(ricciCurvData)
+    classifier = RandomForestClassifier(n_estimators=50, random_state=1)
+    df = ricciCurvData.iloc[: , 1:-1]
+    labels = ricciCurvData.iloc[:,-1:]
+
+    kfResults = cross_val_score(estimator=classifier, X=df, y=labels.values.ravel(), scoring="accuracy", cv=stratKFold)
+    print(kfResults)
+    print(kfResults.mean())
+
+def trainTestRepeatKFold(data):
+
+    repKFold = RepeatedKFold(n_splits=10, n_repeats=10)
+    ricciCurvData = pandas.read_csv(data, header=None)
+    print(ricciCurvData)
+    classifier = RandomForestClassifier(n_estimators=50, random_state=1)
+    df = ricciCurvData.iloc[: , 1:-1]
+    labels = ricciCurvData.iloc[:,-1:]
+  
+    kfResults = cross_val_score(estimator=classifier, X=df, y=labels.values.ravel(), scoring="accuracy", cv=repKFold)
+    print(kfResults)
+    print(kfResults.mean())
+
+def trainTestLoocv(data):
+
+    looCV = LeaveOneOut()
+    ricciCurvData = pandas.read_csv(data, header=None)
+    print(ricciCurvData)
+    df = ricciCurvData.iloc[: , 1:-1]
+    labels = ricciCurvData.iloc[:,-1:]
+
+    classifier = RandomForestClassifier(n_estimators=50, random_state=1)
+
+    print("I'm a really slow pc but i'm computing, gimme time D:")
+    loocvResults = cross_val_score(estimator=classifier, X=df, y=labels.values.ravel(), scoring='accuracy', cv=looCV)
+    print(loocvResults)
+    print(loocvResults.mean())
+
+def trainTestHoldOut(data):
+
+    ricciCurvData = pandas.read_csv(data, header=None)
+    print(ricciCurvData)
+    df = ricciCurvData.iloc[: , 1:-1]
+    labels = ricciCurvData.iloc[:,-1:]
+
+    classifier = RandomForestClassifier(n_estimators=50, random_state=1)
+
+    dfTrain, dfTest, labelsTrain, labelsTest = train_test_split(df, labels, test_size=0.2)
+    classifier.fit(dfTrain, labelsTrain.values.ravel())
+    predictions = classifier.predict(dfTest)
+    accuracy = accuracy_score(labelsTest, predictions)
+    print(accuracy)
+    
+    
